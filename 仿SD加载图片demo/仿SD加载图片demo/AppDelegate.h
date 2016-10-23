cd25d97a9fe886fd5338165d57b964b976bd3b2f
钟昱林 <zhongyulin0131@qq.com>
2016-10-23 20:02:01 +0800

    新建项目

---
+++ 仿SD加载图片demo/仿SD加载图片demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿SD加载图片demo
+//
+//  Created by 钟昱林 on 2016/10/23.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿SD加载图片demo/仿SD加载图片demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿SD加载图片demo
+//
+//  Created by 钟昱林 on 2016/10/23.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
